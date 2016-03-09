00cde913b09264935bb83d7e2ba98955f7dff4ba
tdalsing-pivotal <tdalsing@pivotal.io>
2016-03-08 22:12:27 -0500

    Moved gemfire-r to gemfire-fe-priv

--- gemfire-examples/gemfire-r/gemfire-r-common/src/main/java/io/pivotal/bds/gemfire/r/common/VariableType.java
+++
@@ -1,6 +0,0 @@
-package io.pivotal.bds.gemfire.r.common;
-
-public enum VariableType {
-
-    Query, Model, Evaluate, Matrix, Vector, Train, DTrain, PMML, PMMLPredict
-}
--- gemfire-examples/gemfire-r/gemfire-r-server/locator.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-gfsh start locator --name=locator --bind-address=`hostname`
