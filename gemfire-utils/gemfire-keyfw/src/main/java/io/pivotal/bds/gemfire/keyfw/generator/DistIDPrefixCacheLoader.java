a6b98716b4465fc8473e429f4b656299abc17d8e
tdalsing-pivotal <tdalsing@pivotal.io>
2016-09-16 20:12:45 -0700

    fixed gemfire-keyfw

--- gemfire-utils/gemfire-keyfw/src/main/java/io/pivotal/bds/gemfire/keyfw/generator/DistIDPrefixCacheLoader.java
+++ gemfire-utils/gemfire-keyfw/src/main/java/io/pivotal/bds/gemfire/keyfw/generator/DistIDPrefixCacheLoader.java
@@ -5,27 +5,45 @@ import java.util.Properties;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.gemstone.gemfire.cache.Cache;
+import com.gemstone.gemfire.cache.CacheFactory;
 import com.gemstone.gemfire.cache.CacheLoader;
 import com.gemstone.gemfire.cache.CacheLoaderException;
 import com.gemstone.gemfire.cache.Declarable;
 import com.gemstone.gemfire.cache.LoaderHelper;
+import com.gemstone.gemfire.cache.Region;
+import com.gemstone.gemfire.distributed.DistributedSystem;
 
 public class DistIDPrefixCacheLoader implements CacheLoader<Integer, Long>, Declarable {
-	
-	private static final Logger LOG = LoggerFactory.getLogger(DistIDPrefixCacheLoader.class);
 
-	@Override
-	public void close() {
-	}
+    private IDGenerator<Integer> idGenerator;
+    private static final Logger LOG = LoggerFactory.getLogger(DistIDPrefixCacheLoader.class);
 
-	@Override
-	public void init(Properties props) {
-	}
+    @Override
+    public void close() {
+    }
 
-	@Override
-	public Long load(LoaderHelper<Integer, Long> helper) throws CacheLoaderException {
-		LOG.info("returning 0");
-		return 0L;
-	}
+    @Override
+    public void init(Properties props) {
+    }
 
+    @Override
+    public Long load(LoaderHelper<Integer, Long> helper) throws CacheLoaderException {
+        long id = getIDGenerator().generate("prefix");
+        long prefix = id << 56;
+        LOG.trace("load: id={}, prefix={}", id, prefix);
+        return prefix;
+    }
+
+    private synchronized IDGenerator<Integer> getIDGenerator() {
+        if (idGenerator == null) {
+            LOG.info("getIDGenerator");
+            Cache cache = CacheFactory.getAnyInstance();
+            Region<String, Integer> r = cache.getRegion("prefix");
+            DistributedSystem ds = cache.getDistributedSystem();
+            idGenerator = new ReplicateLockingIntegerIDGenerator(r, ds);
+        }
+
+        return idGenerator;
+    }
 }
