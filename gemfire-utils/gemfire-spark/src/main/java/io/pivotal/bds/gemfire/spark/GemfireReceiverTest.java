c7dd4c5b9633491fd72f23d099404d3781918bb7
tdalsing-pivotal <tdalsing@pivotal.io>
2019-02-06 14:40:54 -0600

    updates dep versions

--- gemfire-utils/gemfire-spark/src/main/java/io/pivotal/bds/gemfire/spark/GemfireReceiverTest.java
+++ gemfire-utils/gemfire-spark/src/main/java/io/pivotal/bds/gemfire/spark/GemfireReceiverTest.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.function.Function;
+import org.apache.spark.api.java.function.VoidFunction;
 import org.apache.spark.storage.StorageLevel;
 import org.apache.spark.streaming.Durations;
 import org.apache.spark.streaming.api.java.JavaDStream;
@@ -20,40 +21,39 @@ import org.apache.geode.cache.RegionShortcut;
 
 public class GemfireReceiverTest {
 
-	private static final Logger LOG = LoggerFactory.getLogger(GemfireReceiverTest.class);
+    private static final Logger LOG = LoggerFactory.getLogger(GemfireReceiverTest.class);
 
-	public static void main(String[] args) throws Exception {
-		LOG.info("Creating context");
-		SparkConf conf = new SparkConf().setAppName("GemfireReceiverTest").setMaster("local[3]");
-		JavaStreamingContext ctx = new JavaStreamingContext(conf, Durations.seconds(10L));
+    public static void main(String[] args) throws Exception {
+        LOG.info("Creating context");
+        SparkConf conf = new SparkConf().setAppName("GemfireReceiverTest").setMaster("local[3]");
+        JavaStreamingContext ctx = new JavaStreamingContext(conf, Durations.seconds(10L));
 
-		LOG.info("Creating cache");
-		Cache cache = new CacheFactory().create();
-		RegionFactory<String, String> regionFactory = cache.createRegionFactory(RegionShortcut.REPLICATE);
-		Region<String, String> region = regionFactory.create("test");
+        LOG.info("Creating cache");
+        Cache cache = new CacheFactory().create();
+        RegionFactory<String, String> regionFactory = cache.createRegionFactory(RegionShortcut.REPLICATE);
+        Region<String, String> region = regionFactory.create("test");
 
-		LOG.info("Creating root");
-		JavaDStream<String> root = ctx
-				.receiverStream(new GemfireReceiver<String, String>(StorageLevel.MEMORY_ONLY(), "test"));
+        LOG.info("Creating root");
+        JavaDStream<String> root = ctx
+                .receiverStream(new GemfireReceiver<String, String>(StorageLevel.MEMORY_ONLY(), "test"));
 
-		LOG.info("Executing foreach");
-		root.foreach(new Function<JavaRDD<String>, Void>() {
+        LOG.info("Executing foreach");
+        root.foreachRDD(
+                new VoidFunction<JavaRDD<String>>() {
+                    private static final long serialVersionUID = 1L;
 
-			private static final long serialVersionUID = 1L;
+                    @Override
+                    public void call(JavaRDD<String> v1) throws Exception {
+                        LOG.error("v1={}", v1);
+                        List<String> l = v1.collect();
+                        for (String s : l) {
+                            LOG.error(" s={}", s);
+                        }
+                    }
+                });
 
-			@Override
-			public Void call(JavaRDD<String> v1) throws Exception {
-				LOG.error("v1={}", v1);
-				List<String> l = v1.collect();
-				for (String s : l) {
-					LOG.error(" s={}", s);
-				}
-				return null;
-			}
-		});
-
-		LOG.info("Starting");
-		ctx.start();
-		ctx.awaitTermination();
-	}
+        LOG.info("Starting");
+        ctx.start();
+        ctx.awaitTermination();
+    }
 }
